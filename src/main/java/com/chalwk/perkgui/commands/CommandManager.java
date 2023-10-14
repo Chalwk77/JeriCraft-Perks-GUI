80ff1db3c086cecff39a5264a8ba3c8993785e5a
Chalwk77 <jericho.crosby227@gmail.com>
2023-10-15 00:19:00 +1300

    initial commit

--- src/main/java/com/chalwk/perkgui/commands/CommandManager.java
+++ src/main/java/com/chalwk/perkgui/commands/CommandManager.java
@@ -6,14 +6,17 @@ import com.chalwk.perkgui.commands.subcommands.ShowPerks;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.chalwk.perkgui.Main.send;
 
-public class CommandManager implements CommandExecutor {
+public class CommandManager implements CommandExecutor, TabCompleter {
 
     private final ArrayList<SubCommand> subcommands = new ArrayList<>();
 
@@ -35,9 +38,9 @@ public class CommandManager implements CommandExecutor {
                         return true;
                     }
                 }
-                send(player, "&cInvalid Syntax. Usage: &b/perks show|reload");
+                send(player, "&cInvalid Syntax. Usage: &b/perks show/reload");
             } else {
-                send(player, "&cInvalid Syntax. Usage: &b/perks show|reload");
+                send(player, "&cInvalid Syntax. Usage: &b/perks show/reload");
             }
         }
         return true;
@@ -55,5 +58,17 @@ public class CommandManager implements CommandExecutor {
             send(player, errorMessage);
             return false;
         }
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        ArrayList<String> tab = new ArrayList<>();
+        if (args.length == 1) {
+            for (SubCommand subCommand : getSubCommands()) {
+                tab.add(subCommand.getName());
+            }
+            return tab;
+        }
+        return tab;
     }
 }
